3506002613924280ae787bc4dd8c26865e431235
David Isebarn <isebarn182@gmail.com>
2018-11-02 19:01:58 +0000

    basic stuff

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from threading import Lock
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+from app import app, socketio
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
