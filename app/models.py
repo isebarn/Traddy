b1bf04a5ecb6f572e3495750b658466a73bdd9c8
David Isebarn <isebarn182@gmail.com>
2018-11-04 15:03:23 +0000

    re-create db

--- app/models.py
+++ app/models.py
@@ -1,18 +1,27 @@
 from app import db
 from datetime import datetime
+from sqlalchemy import ForeignKey
+
+class Pairs(db.Model):
+	pair_id = db.Column(db.Integer, primary_key=True)
+	pair = db.Column(db.String(64), primary_key=True)
+	units_per_pip_usd = db.Column(db.Numeric(precision=7, scale=2))
+	comission = db.Column(db.Numeric(precision=4, scale=2))
+
 
 class Orders(db.Model):
 	order_id = db.Column(db.Integer, primary_key=True)
 	pair_id = db.Column(db.Integer)
-	is_long = db.Column(db.Boolean)
+	units = db.Column(db.Integer)
 	enter = db.Column(db.DateTime, default=datetime.utcnow)
 	exit = db.Column(db.DateTime)
-	enter_price = db.Column(db.Numeric(precision=5, asdecimal=False, decimal_return_scale=None))
-	exit_price = db.Column(db.Numeric(precision=5, asdecimal=False, decimal_return_scale=None))
-	exit_PIP = db.Column(db.Numeric(precision=2, asdecimal=False, decimal_return_scale=None))
-	units = db.Column(db.Integer)
-	spread = db.Column(db.Numeric(precision=2, asdecimal=False, decimal_return_scale=None))
-	SL = db.Column(db.Numeric(precision=2, asdecimal=False, decimal_return_scale=None))
-	TP = db.Column(db.Numeric(precision=2, asdecimal=False, decimal_return_scale=None))
-	opportunity_type = db.Column(db.Integer)
+	price = db.Column(db.Numeric(precision=8, scale=5, asdecimal=False, decimal_return_scale=None))
+	PIP = db.Column(db.Numeric(precision=5, scale=2, asdecimal=False, decimal_return_scale=None))
+
+
+class LimitHistory(db.Model):
+	limit_id = db.Column(db.Integer, primary_key=True)
+	order_id = db.Column(db.Integer)
+	sl = db.Column(db.Numeric(precision=7, scale=5, asdecimal=False, decimal_return_scale=None))
+	tp = db.Column(db.Numeric(precision=7, scale=5, asdecimal=False, decimal_return_scale=None))
 
