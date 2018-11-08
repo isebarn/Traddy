d785585be8d471cc1779cd11f4dafabc733a1829
David Isebarn <isebarn182@gmail.com>
2018-11-08 22:19:06 +0000

    API fetch price

--- app/sockets.py
+++ app/sockets.py
@@ -7,9 +7,12 @@ from app.calc import SL
 
 from app.models import Pairs
 
+from urllib.parse import urlencode, quote_plus
+import urllib.request
+
 
 @socketio.on('SLCalc', namespace='/test')
-def test_message(data):
+def SLCalc(data):
 	data['units_per_pip'] = 1e4
 	data = dict((k,float(v)) for k,v in data.items())
 
@@ -20,7 +23,7 @@ def test_message(data):
 		 broadcast=False) 
 
 @socketio.on('Order', namespace='/test')
-def test_message(data):
+def Order(data):
 
 	emit('order',
 		 data,
@@ -29,7 +32,7 @@ def test_message(data):
 
 
 @socketio.on('request_ui_data', namespace='/test')
-def test_message(data):
+def request_ui_data(data):
 	pairs = Pairs.query.all()
 
 	result = [{'id': pair.pair_id, 'pair': pair.pair, 'units_per_pip': float(pair.units_per_pip_usd), 'comission': float(pair.comission)} for pair in pairs]
@@ -37,3 +40,19 @@ def test_message(data):
 	emit('ui_data',
 		 json.dumps(result),
 		 broadcast=False)	
+
+@socketio.on('request_pair_price', namespace='/test')
+def request_pair_price(data):
+	pair = data['pair']
+	api_key = app.config.get('FOREX_API')
+	base_url = "https://forex.1forge.com/1.0.3/quotes?"
+
+	url = base_url + urlencode({"pairs": pair, "api_key": api_key})	
+	response = urllib.request.urlopen(url)
+	price = json.loads(response.read())[0]['bid']
+
+	emit('serve_pair_price',
+		 {'price': price},
+		 broadcast=False)
+	
+
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config(object):
 	# ...
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+	FOREX_API = os.environ.get('FOREX_API')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 	DEBUG=True
